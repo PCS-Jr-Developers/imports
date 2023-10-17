d37b8e46354d6181824007310b2e5ede03eb9898
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-10-17 02:14:47 -0400

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,11 @@
+import math
+
+# Round a number up
+num1 = 4.3
+rounded_up = math.ceil(num1)
+print("Rounded up:", rounded_up)  # Output: Rounded up: 5
+
+# Round a number down
+num2 = 9.8
+rounded_down = math.floor(num2)
+print("Rounded down:", rounded_down)  # Output: Rounded down: 9
